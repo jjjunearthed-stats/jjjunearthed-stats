b6caf004b3ce5312c4fcc7bb3d2f28c7049a599f
Craig Curtis <craig.d.curtis@gmail.com>
2017-03-08 14:22:34 +1000

    Update charts.js

--- docs/scripts/charts.js
+++ docs/scripts/charts.js
@@ -7,7 +7,7 @@ window.onload = function() {
   // instantiates the pie chart, passes in the data and
   // draws it.
   function drawChart() {
-    $.getJSON('../data/artistByLocation.json', function(jsonData) {
+    $.getJSON('data/artistByLocation.json', function(jsonData) {
       var options = {
         region: 'AU',
         displayMode: 'markers',
