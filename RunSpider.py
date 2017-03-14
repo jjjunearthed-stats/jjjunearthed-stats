f77ef4b845322154f737df506e59ce9022bb9fbd
craigles <craig.d.curtis@gmail.com>
2017-03-14 20:09:25 +1000

    run spider with args

--- RunSpider.py
+++ RunSpider.py
@@ -1,6 +1,5 @@
 from scrapy import cmdline
-
 import File
 
 File.delete_content("artists.json")
-cmdline.execute("scrapy runspider JJJUnearthed/spiders/JJJUnearthedSpider.py -t json -o artists.json".split())
+cmdline.execute("scrapy runspider JJJUnearthed/spiders/JJJUnearthedSpider.py -a from_index=4269 -a to_index=4270 -t json -o artists.json".split())
