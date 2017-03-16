72876ddb1d47378dc257d1595cbae11007be0bb9
Craig Curtis <craig.d.curtis@gmail.com>
2017-03-16 19:59:58 +1000

    Update charts.js

--- docs/scripts/charts.js
+++ docs/scripts/charts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    google.charts.load('current', {packages:['geochart']});
+    google.charts.load('current', {packages:['geochart', 'table']});
     google.charts.setOnLoadCallback(drawCharts);
 
   function drawCharts() {
@@ -13,6 +13,22 @@ $(document).ready(function() {
             var chart = new google.visualization.GeoChart(chartElement);
 
             chart.draw(data, options);
+          });
+      });
+      
+      $(".table").each(function() {
+          var dataUrl = $(this).attr('data-url');
+          
+          $.getJSON(dataUrl, function(json) {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Location');
+            data.addColumn('number', 'Number of bands');
+            data.addColumn('number', 'Number of bands per 100 000 people');
+            data.addRows(json);
+
+            var table = new google.visualization.Table(document.getElementById('table_div'));
+
+            table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
           });
       });
   }
