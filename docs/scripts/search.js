c5d84402e7d053727d9b46cc8b16d21983a2a3f8
Craig Curtis <craig.d.curtis@gmail.com>
2017-03-25 08:26:54 +1000

    Update search.js

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var database = firebase.database();
-    database.ref("6147").once("value").then(function(snapshot) {
-        alert(snapshot);
+    database.ref().orderByValue().on("name", function(snapshot) {
+        snapshot.forEach(function(data) {
+            alert("The " + data.key + " score is " + data.val());
+      });
     });
 });
