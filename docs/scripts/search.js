dfeb0957b50eec46a0da979f24876c5d5a717e4e
Craig Curtis <craig.d.curtis@gmail.com>
2017-03-25 08:40:49 +1000

    Update search.js

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var database = firebase.database();
-    database.ref().orderByChild("name").on("value", function(snapshot) {
+    database.ref().orderByChild("name").startAt("Courtney Barnett").endAt("Courtney Barnett").on("value", function(snapshot) {
         snapshot.forEach(function(data) {
             alert("The " + data.key + " score is " + data.val());
       });
