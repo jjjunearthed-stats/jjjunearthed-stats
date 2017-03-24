c05168e8d7084aedd9b83926cbc1747a955cb001
Craig Curtis <craig.d.curtis@gmail.com>
2017-03-25 08:18:35 +1000

    Update search.js

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var database = firebase.database();
-    database.ref("6147").once("name").then(function(snapshot) {
+    database.ref("6147").once("value").then(function(snapshot) {
         alert(snapshot);
     });
 });
