902383aaca000094413100401aeac899bc0355bf
Craig Curtis <craig.d.curtis@gmail.com>
2017-04-11 13:00:07 +1000

    Delete RunSpider.py

--- RunSpider.py
+++
@@ -1,5 +0,0 @@
-from scrapy import cmdline
-import File
-
-File.delete_content("artists.json")
-cmdline.execute("scrapy runspider JJJUnearthed/spiders/JJJUnearthedSpider.py -a from_index=4269 -a to_index=4270 -t json -o artists.json".split())
