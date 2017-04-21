ef9ed008af312fc3e6e18e3d5e4a93b6bfd960b5
craigles <craig.d.curtis@gmail.com>
2017-04-21 11:23:45 +1000

    More stats

--- CrunchStats.py
+++ CrunchStats.py
@@ -1,6 +1,8 @@
 import json
 import pandas
 import File
+from datetime import datetime
+from itertools import groupby
 
 
 class ArtistStats:
@@ -17,21 +19,25 @@ class ArtistStats:
 
         return None
 
+    @staticmethod
+    def to_date(date):
+        return datetime.strptime(date, "%Y-%m-%d")
+
     def by_location(self):
-        group_by_location = pandas.DataFrame(self.artists).groupby(["location"])
+        group_by_location = pandas.DataFrame(self.artists).groupby("location").size()
 
         data = [["City", "Number of artists"]]
-        for name, group in group_by_location:
-            data.append([name, len(group)])
+        for name, size in group_by_location.iteritems():
+            data.append([name, int(size)])
 
         return data
 
     def per_capita(self):
-        group_by_location = pandas.DataFrame(self.artists).groupby(["location"])
+        group_by_location = pandas.DataFrame(self.artists).groupby("location").size()
 
         data = [["Location", "Artists per 100000 people"]]
-        for name, group in group_by_location:
-            location_per_capita = self.per_capita_by_location(name, len(group))
+        for name, size in group_by_location.iteritems():
+            location_per_capita = self.per_capita_by_location(name, int(size))
 
             if location_per_capita is not None:
                 data.append([name, location_per_capita])
@@ -39,12 +45,41 @@ class ArtistStats:
         return data
 
     def location_table(self):
-        group_by_location = pandas.DataFrame(self.artists).groupby(["location"])
-
+        group_by_location = pandas.DataFrame(self.artists).groupby("location").size()
         data = []
-        for name, group in group_by_location:
-            data.append([name, len(group), self.per_capita_by_location(name, len(group))])
+        for name, size in group_by_location.iteritems():
+            data.append([name, int(size), self.per_capita_by_location(name, int(size))])
 
+        return data
+
+    def percentage_of_artists(self, track_condition=lambda t: True):
+        artists_played_on_jjj = [a for a in self.artists
+                                 for t in filter(track_condition, a["tracks"])]
+
+        percentage = len(artists_played_on_jjj) / len(self.artists) * 100
+        return round(percentage, 2)
+
+    def genre_percentages(self):
+        genres = [g for a in self.artists for g in a["genre"]]
+        number_of_genres = len(genres)
+
+        data = [["Genre", "Percentage"]]
+        for name, group in groupby(sorted(genres)):
+            genre_percentage = len(list(group)) / number_of_genres * 100
+            data.append([name, round(genre_percentage, 2)])
+
+        return data
+
+    def genres_per_year(self, track_condition=lambda t: True):
+        genre_years = [{"genre": g, "year": self.to_date(t["date"]).year}
+                       for a in self.artists for g in a["genre"]
+                       for t in filter(track_condition, a["tracks"])]
+
+        grouping = pandas.DataFrame(genre_years).groupby(["year", "genre"]).size()
+
+        data = [["Year", "Genre", "Number Of Tracks"]]
+        for group, size in grouping.iteritems():
+            data.append([group[0], group[1], int(size)])
         return data
 
     def hierarchial_graph(self):
@@ -75,7 +110,9 @@ class ArtistStats:
             "TotalNumberOfArtists": len(self.artists),
             "TotalNumberOfTracks": len(tracks),
             "FromDate": tracks["date"].min(),
-            "ToDate": tracks["date"].max()
+            "ToDate": tracks["date"].max(),
+            "PercentageOfArtistsPlayedOnJJJ": self.percentage_of_artists(lambda t: t["played_on_jjj"]),
+            "PercentageOfArtistsPlayedOnUnearthed": self.percentage_of_artists(lambda t: t["played_on_unearthed"]),
         }
 
 
@@ -86,3 +123,7 @@ File.write_file("docs/data/artistsByLocation.json", stats.by_location())
 File.write_file("docs/data/artistsPerCapita.json", stats.per_capita())
 File.write_file("docs/data/artistsLocationTable.json", stats.location_table())
 File.write_file("docs/_data/stats.json", stats.stats())
+File.write_file("docs/data/genrePercentages.json", stats.genre_percentages())
+File.write_file("docs/data/genresPerYear.json", stats.genres_per_year())
+File.write_file("docs/data/genresPlayedOnJJJPerYear.json", stats.genres_per_year(lambda t: t["played_on_jjj"]))
+File.write_file("docs/data/genresPlayedOnUnearthedPerYear.json", stats.genres_per_year(lambda t: t["played_on_unearthed"]))
