719f5e63ab4f265e304cf0afcad6985cf408545f
craigles <craig.d.curtis@gmail.com>
2017-04-21 17:02:03 +1000

    genre line chart

--- File.py
+++ File.py
@@ -13,15 +13,13 @@ def write_file(file_path, data):
     delete_content(file_path)
 
     with open(file_path, "w") as dataFile:
-        dataFile.write(json.dumps(data, cls=MyEncoder))
+        dataFile.write(json.dumps(data, cls=Encoder))
         dataFile.close()
 
 
-class MyEncoder(json.JSONEncoder):
+class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, numpy.int64):
             return int(obj)
-        elif isinstance(obj, numpy.float64):
-            return float(obj)
         else:
-            return super(MyEncoder, self).default(obj)
+            return super(Encoder, self).default(obj)
