bc5a7155bce52e96ddf7be7cd4df78fe9ef6afb9
craigles <craig.d.curtis@gmail.com>
2017-06-10 14:22:01 +1000

    Update using unobtrusive Google charts

---
+++ docs/_site/scripts/chartconfig.js
@@ -0,0 +1,14 @@
+unobtrusiveGoogleCharts.options.geoMap = {
+    region: 'AU',
+    displayMode: 'markers',
+    colors: ['blue', 'red']
+};
+
+unobtrusiveGoogleCharts.options.bar = {
+    orientation: "vertical",
+    legend: { position: "none" }
+};
+
+unobtrusiveGoogleCharts.options.line = {
+    curveType: "function"
+}
---
+++ docs/_site/search.md
@@ -0,0 +1,4 @@
+<input type="text" id="xPath" name="xPath"/>
+<button id="Search">Search</button>
+<div id="results"></div>
+<script type="text/javascript" src="scripts/search.js"/>
