c3bd26a9c5479e8eda3f9ae61347cb562e42f6dc
craigles <craig.d.curtis@gmail.com>
2017-09-17 10:09:09 +1000

    Use unobtrusive-google-charts

--- docs/scripts/search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var database = firebase.database();
-    database.ref().orderByChild("name").startAt("YELLERS").endAt("YELLERS").on("value", function(snapshot) {
-        snapshot.forEach(function(data) {
-            alert("The " + data.key + " score is " + data.val());
-      });
-    });
-});
